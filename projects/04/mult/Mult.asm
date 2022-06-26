410d6c390d80f18e4ea6db3242b2e45769759f9a
Gakuto Watanabe <g_watanabe@ga-tech.co.jp>
2022-06-27 02:01:58 +0900

    create Mult.asm

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -5,8 +5,40 @@
 
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
-//
-// This program only needs to handle arguments that satisfy
-// R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
-// Put your code here.
+@i
+M=1
+
+@R0
+D=M
+
+@R2
+M=D
+
+(LOOP)
+@R1
+D=M
+
+@i
+D=D-M
+
+@END
+D; JLE // R1 - i <= 0 => 終了
+
+@R0
+D=M
+
+// sumにR0を足す
+@R2
+M=D+M
+
+// loopの最後にiをインクリメント
+@i
+M=M+1
+
+@LOOP
+0; JMP
+
+(END)
+@END
+0; JMP
